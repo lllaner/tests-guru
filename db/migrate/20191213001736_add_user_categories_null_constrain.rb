a948dd4dacbee03a0e9cab1583c7f0d2cf6a9e62
Laner <iifunnymomen@gmail.com>
2019-12-13 02:19:02 +0200

    add categories not null

---
+++ db/migrate/20191213001736_add_user_categories_null_constrain.rb
@@ -0,0 +1,5 @@
+class AddUserCategoriesNullConstrain < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null(:categories, :title, false)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_001003) do
+ActiveRecord::Schema.define(version: 2019_12_13_001736) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_001003) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
