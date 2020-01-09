f430bb65df0e8be05ecd062a1295c33d4ea6bf02
Laner <iifunnymomen@gmail.com>
2020-01-09 14:35:15 +0200

    make lesson associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :my_tests, class_name: "Test", foreign_key: "author_id"
+  has_and_belongs_to_many :tests
   def level_test(level)
       Test.where(level: level).joins('INNER JOIN user_tests ON user_tests.test_id = tests.id').where('user_tests.user_id' => self.id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_184712) do
+ActiveRecord::Schema.define(version: 2020_01_09_122045) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,7 +41,14 @@ ActiveRecord::Schema.define(version: 2019_12_23_184712) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.integer "author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
   end
 
   create_table "user_tests", force: :cascade do |t|
