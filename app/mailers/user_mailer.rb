868863c56533a6a66007e691eeb2a7c59defdc74
Laner <iifunnymomen@gmail.com>
2020-02-21 10:01:34 +0200

    deleted mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,7 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: 'iifunnymomen@gmail.com'
-  def welcome_email
-    mail(to: 'ilanercode@gmail.com', subject: 'Welcome to My Awesome Site')
-  end
-
-end
