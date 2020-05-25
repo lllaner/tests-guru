28a931c442c0c59c3086c63db51eb039a818942f
Laner <iifunnymomen@gmail.com>
2020-05-26 02:56:50 +0300

    remove space tab

--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -1,7 +1,7 @@
 class TestsMailer < ApplicationMailer
-    def completed_test(test_passage)
-        @user = test_passage.user
-        @test = test_passage.test
-        mail to: @user.email
-    end
+  def completed_test(test_passage)
+    @user = test_passage.user
+    @test = test_passage.test
+    mail to: @user.email
+  end
 end
