48c4500c755fa639c4cee9c3bbcd0d83be0acaed
Laner <iifunnymomen@gmail.com>
2020-08-26 02:46:21 +0300

    add-feedback-form-email

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -2,6 +2,7 @@ class FeedbacksMailer < ApplicationMailer
   def feedback_email(feedback)
     @name = feedback.name
     @message = feedback.message
+    @email = feedback.email
     mail from: feedback.email, to: ENV['ADMIN_EMAIL']
   end
 end
