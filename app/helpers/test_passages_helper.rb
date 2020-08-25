d69275583c060e63fb19bc6c832498afcb227fc6
Laner <iifunnymomen@gmail.com>
2020-08-25 15:45:41 +0300

    add-progress-bar

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -2,4 +2,8 @@ module TestPassagesHelper
   def position_number
     "#{@test_passage.current_number_question} / #{@test_passage.all_questions}"
   end
+
+  def progress
+    ((@test_passage.current_number_question.to_f - 1) / @test_passage.all_questions * 100).to_i
+  end
 end
